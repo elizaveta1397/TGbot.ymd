d247c5aee1905eef4844e7df36d453d33cdb2e0d
Elizaveta <elizsaratova@gmail.com>
2026-07-09 09:12:17 +0200

    Fix await in back_to_frames

--- handlers/cinemalogy/back_to_frames.py
+++ handlers/cinemalogy/back_to_frames.py
@@ -1,20 +1,21 @@
 from services.cinemalogy.materials import get_material
 
-current_frame = get_parameter(
-    telegram_id,
-    "cinemalogy_current_frame"
-)
+async def back_to_frames(callback, telegram_id):
+    current_frame = get_parameter(
+        telegram_id,
+        "cinemalogy_current_frame"
+    )
 
-image = get_material(
-    f"cinemalogy_frame_{int(current_frame):02d}_image"
-)
+    image = get_material(
+        f"cinemalogy_frame_{int(current_frame):02d}_image"
+    )
 
-text = get_material(
-    f"cinemalogy_frame_{int(current_frame):02d}_text"
-)
+    text = get_material(
+        f"cinemalogy_frame_{int(current_frame):02d}_text"
+    )
 
-await callback.message.answer_photo(
-    photo=image["telegram_file_id"],
-    caption=text["text"] if text else "",
-    reply_markup=frame_keyboard()
-)
+    await callback.message.answer_photo(
+        photo=image["telegram_file_id"],
+        caption=text["text"] if text else "",
+        reply_markup=frame_keyboard()
+    )
